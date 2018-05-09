8686bf369e90a3d18c44eaba37ea31391496a62d
scaler91 <scaler91@hotmail.com>
2018-05-09 10:41:47 +0200

    Merge origin/master
    Conflicts:
    	Hack&Beers/src/hack/beers/Usuario.java

--- Hack&Beers/src/hack/beers/vista/LoginV.java
+++ Hack&Beers/src/hack/beers/vista/LoginV.java
@@ -22,7 +22,7 @@ public class LoginV extends javax.swing.JFrame {
      */
     public LoginV() {
         initComponents();
-        Usuario u = new Usuario();
+        
         
         
     }
@@ -127,7 +127,7 @@ public class LoginV extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        if
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
