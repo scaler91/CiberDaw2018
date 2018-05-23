e6ccf351e08babab99a9a7ab14501aec81f3da13
scaler91 <scaler91@hotmail.com>
2018-05-23 11:07:56 +0200

    Merge origin/master
    Conflicts:
    	Hack&Beers/src/hack/beers/vista/AdministradorV.form
    	Hack&Beers/src/hack/beers/vista/AdministradorV.java

--- Hack&Beers/src/hack/beers/vista/ClienteV.java
+++ Hack&Beers/src/hack/beers/vista/ClienteV.java
@@ -27,7 +27,7 @@ public class ClienteV extends javax.swing.JFrame {
     LoginV v;
     String[] cabecera = {"Nombre", "Cantidad"};
     Timer timer;
-    int tiempo = 500000;
+    int tiempo = 300000;
     int minuto = tiempo / 60000;
     int segundo = tiempo % 60000;
 // Import ImageIcon     
